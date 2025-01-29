dbc5fe2044c2d2212be61e584fdd42d50309bd9f
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-01-29 18:56:10 +0300

    email instead of username

--- Controllers/Contracts/LoginRequest.cs
+++ Controllers/Contracts/LoginRequest.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 public class LoginRequest
 {
     [Required]
-    public string Username { get; set; } = null!;
+    public string Email { get; set; } = null!;
 
     [Required]
     public string Password { get; set; } = null!;
