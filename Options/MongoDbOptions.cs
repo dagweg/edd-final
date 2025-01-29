1bebf7da9506efbe0b3da89bae558d4a6cc2f2e2
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-01-29 18:41:01 +0300

    ft: mongodb options

---
+++ Options/MongoDbOptions.cs
@@ -0,0 +1,8 @@
+namespace HouseRentalSystem.Options;
+
+public class MongoDbOptions
+{
+    public static string SectionName = "MongoDbOptions";
+    public string? ConnectionString { get; init; }
+    public string? DatabaseName { get; init; }
+}
