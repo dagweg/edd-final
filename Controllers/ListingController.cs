5c2d9b7f84fceb860caecb504c33244a9b6ec4a2
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-01-30 16:45:57 +0300

    final

--- Controllers/ListingController.cs
+++ Controllers/ListingController.cs
@@ -1,11 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using HouseRentalSystem.Attributes;
 using HouseRentalSystem.Controllers.Contracts;
-using HouseRentalSystem.Services;
 using HouseRentalSystem.Helpers;
+using HouseRentalSystem.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HouseRentalSystem.Controllers;
 
+/// <summary>
+/// Controller for managing house rental listings.
+/// </summary>
+/// <response code="401">Unauthorized. User is not authenticated.</response>
+/// <response code="500">Internal server error. Something went wrong on the server.</response>
 [Authorize]
 [ApiController]
 [Route("listings")]
@@ -32,55 +39,75 @@ public class ListingController : ControllerBase
     {
         var userId = JwtTokenHelper.GetUserId(User);
 
-        if (userId is null)
-            return Unauthorized();
-
         await _listingService.AddListingAsync(createListingRequest, userId);
 
         return Created();
     }
 
+    /// <summary>
+    /// Get all listings.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <response code="200">Returns all listings.</response>
+    /// <response code="401">Unauthorized. User is not authenticated.</response>
+    /// <response code="500">Internal server error. Something went wrong on the server.</response>
     [HttpGet("{id}")]
-    public async Task<IActionResult> GetListingById(string id)
+    public async Task<IActionResult> GetListingById(
+        [FromRoute] [Required] [ObjectIdValidation] string id
+    )
     {
         var listing = await _listingService.GetListingByIdAsync(id);
         return Ok(listing);
     }
 
+    /// <summary>
+    /// Delete a listing by ID.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <response code="204">The listing was successfully deleted.</response>
+    /// <response code="401">Unauthorized. User is not authenticated.</response>
     [HttpDelete("{id}")]
-    public async Task<IActionResult> DeleteListingById(string id)
+    public async Task<IActionResult> DeleteListingById(
+        [FromRoute] [Required] [ObjectIdValidation] string id
+    )
     {
         var userId = JwtTokenHelper.GetUserId(User);
-        if (userId is null)
-            return Unauthorized();
 
-        // First get the listing to check if the user is the owner
-        try
-        {
-            var listing = await _listingService.GetListingByIdAsync(id);
-            if (listing.HostId != userId)
-                return Forbid("You can only delete your own listings");
+        await _listingService.DeleteListingAsync(id);
 
-            var deleted = await _listingService.DeleteListingAsync(id);
-            if (!deleted)
-                return NotFound($"Listing with ID {id} not found");
+        return NoContent();
+    }
 
-            return NoContent();
-        }
-        catch (KeyNotFoundException)
-        {
-            return NotFound($"Listing with ID {id} not found");
-        }
-
-        /// <summary>
-    /// Gets a list of listings by a name.
+    /// <summary>
+    /// Filters the house listings by different parameters.
     /// </summary>
-    /// <param name="name">The name of the listing to get.</param>
+    /// <param name="filterBy"></param>
+    /// <param name="value"></param>
+    /// <param name="paginationParameters"></param>
     /// <returns>A list of listings with the name that was searched.</returns>
-    [HttpGet("search/{name}")]
-    public async Task<IActionResult> GetListingByName(string name)
+    [HttpGet("search")]
+    public async Task<IActionResult> FilterListing(
+        [FromQuery] string value,
+        [FromQuery] PaginationParameters paginationParameters,
+        [FromQuery] string filterBy = "title"
+    )
     {
-        var listings = await _listingService.GetListingByNameAsync(name);
+        var listings = await _listingService.FilterListingAsync(
+            filterBy,
+            value,
+            paginationParameters
+        );
         return Ok(listings);
+    }
+
+    [HttpPatch("{id}")]
+    public async Task<IActionResult> PatchListing(
+        [FromRoute] [Required] [ObjectIdValidation] string id,
+        [FromBody] PatchListingRequest patchListingRequest
+    )
+    {
+        var listing = await _listingService.PatchListingAsync(id, patchListingRequest);
+
+        return Ok(listing);
     }
 }
